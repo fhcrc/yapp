593b18a35848baeb4b17e63538a85849a5357761
Noah Hoffman <noah.hoffman@gmail.com>
2014-04-14 14:59:09 -0700

    - add build_wheels.sh - bootstrap uses wheels - update requirements

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Usage: bin/bootstrap.sh [Options]
 
 # Create a virtualenv, and install requirements to it.
@@ -9,21 +8,25 @@
 
 set -e
 
-venv=$(basename $(pwd))-env
+GREP_OPTIONS=--color=never
+VENV=$(basename $(pwd))-env
 YAPP=$(cd $(dirname $BASH_SOURCE) && cd .. && pwd)
 PYTHON=$(which python)
+PY_VERSION=$($PYTHON -c 'import sys; print "{}.{}.{}".format(*sys.version_info[:3])')
 PPLACER_VERSION=binary
-PPLACER_BINARY_VERSION=1.1
-PPLACER_BUILD=1.1.alpha15
-VENV_VERSION=1.11
-INFERNAL_VERSION=1.1
+WHEELSTREET=$(readlink -f ~/wheelstreet)
+REQFILE=$YAPP/requirements.txt
 
 if [[ $1 == '-h' || $1 == '--help' ]]; then
     echo "Create a virtualenv and install all pipeline dependencies"
     echo "Options:"
-    echo "--venv            - path of virtualenv [$venv]"
+    echo "--venv            - path of virtualenv [$VENV]"
     echo "--python          - path to an alternative python interpreter [$PYTHON]"
     echo "--pplacer-version - git branch, tag, or sha (or 'binary' to use latest binaries) [$PPLACER_VERSION]"
+    echo "--wheelstreet     - path to directory containing python wheels; wheel files will be"
+    echo "                    in a subdirectory named according to the python interpreter version"
+    echo "                    (eg '2.7.5') [$WHEELSTREET]"
+    echo "--requirements    - an alternative requiremets file [$REQFILE]"
     exit 0
 fi
 
@@ -32,9 +35,22 @@ while true; do
 	--venv ) venv="$2"; shift 2 ;;
 	--python ) PYTHON="$2"; shift 2 ;;
 	--pplacer-version ) PPLACER_VERSION="$2"; shift 2 ;;
+	--wheelstreet ) WHEELSTREET=$(readlink -f "$2"); shift 2 ;;
+	--requirements ) REQFILE="$2"; shift 2 ;;
 	* ) break ;;
     esac
 done
+
+PPLACER_BINARY_VERSION=1.1
+PPLACER_BUILD=1.1.alpha15
+VENV_VERSION=1.11.4
+INFERNAL_VERSION=1.1
+WHEELHOUSE=
+
+if [[ ! -z $WHEELSTREET ]]; then
+    WHEELHOUSE=$WHEELSTREET/$PY_VERSION
+    test -d $WHEELHOUSE || (echo "cannot access $WHEELHOUSE"; exit 1)
+fi
 
 mkdir -p src
 
@@ -42,27 +58,37 @@ mkdir -p src
 # source. This also provides setuptools and pip. Inspired by
 # http://eli.thegreenplace.net/2013/04/20/bootstrapping-virtualenv/
 
-VENV_URL='http://pypi.python.org/packages/source/v/virtualenv'
-
-# download virtualenv source if necessary
-if [ ! -f src/virtualenv-${VENV_VERSION}/virtualenv.py ]; then
-    (cd src && \
-	wget -N ${VENV_URL}/virtualenv-${VENV_VERSION}.tar.gz && \
-	tar -xf virtualenv-${VENV_VERSION}.tar.gz)
-fi
-
 # create virtualenv if necessary
-if [ ! -f $venv/bin/activate ]; then
-    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py $venv
-    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py --relocatable $venv
+if [ ! -f ${VENV:?}/bin/activate ]; then
+    # download virtualenv source if necessary
+    if [ ! -f src/virtualenv-${VENV_VERSION}/virtualenv.py ]; then
+	VENV_URL='https://pypi.python.org/packages/source/v/virtualenv'
+	(cd src && \
+	    wget -N ${VENV_URL}/virtualenv-${VENV_VERSION}.tar.gz && \
+	    tar -xf virtualenv-${VENV_VERSION}.tar.gz)
+    fi
+    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py $VENV
+    $PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py --relocatable $VENV
 else
-    echo "found existing virtualenv $venv"
+    echo "found existing virtualenv $VENV"
 fi
 
-source $venv/bin/activate
+source $VENV/bin/activate
+
+# install python packages from pipy or wheels
+grep -v -E '^#|git+' $REQFILE | while read pkg; do
+    if [[ -z $WHEELHOUSE ]]; then
+	pip install --allow-external argparse $pkg
+    else
+	pip install --allow-external argparse --use-wheel --find-links=$WHEELHOUSE $pkg
+    fi
+done
+
+# install packages from git repos
+pip install -r <(grep git+ $REQFILE | grep -v -E '^#')
 
 # scons can't be installed using pip
-if [ ! -f $venv/bin/scons ]; then
+if [ ! -f $VENV/bin/scons ]; then
     (cd src && \
 	wget -N http://downloads.sourceforge.net/project/scons/scons/2.3.0/scons-2.3.0.tar.gz && \
 	tar -xf scons-2.3.0.tar.gz && \
@@ -73,30 +99,29 @@ else
     echo "scons is already installed in $(which scons)"
 fi
 
-opamroot=$HOME/local/share/opam
-
 if [[ $PPLACER_VERSION == "binary" ]]; then
     function srcdir(){
 	tar -tf $1 | head -1
     }
 
     PPLACER_TGZ=pplacer-v${PPLACER_BINARY_VERSION}-Linux.tar.gz
-    if ! $venv/bin/pplacer --version | grep -q "$PPLACER_BUILD"; then
+    if ! $VENV/bin/pplacer --version | grep -q "$PPLACER_BUILD"; then
 	mkdir -p src && \
 	    (cd src && \
 	    wget -N http://matsen.fhcrc.org/pplacer/builds/$PPLACER_TGZ && \
 	    tar -xf $PPLACER_TGZ && \
-	    cp $(srcdir $PPLACER_TGZ)/{pplacer,guppy,rppr} ../$venv/bin && \
+	    cp $(srcdir $PPLACER_TGZ)/{pplacer,guppy,rppr} ../$VENV/bin && \
 	    pip install -U $(srcdir $PPLACER_TGZ)/scripts && \
 	    rm -r $(srcdir $PPLACER_TGZ))
     else
 	echo -n "pplacer is already installed: "
-	$venv/bin/pplacer --version
+	$VENV/bin/pplacer --version
     fi
 else
-    echo "installing opam and ocaml in $opamroot and pplacer in $venv/bin"
+    opamroot=$HOME/local/share/opam
+    echo "installing opam and ocaml in $opamroot and pplacer in $VENV/bin"
     bin/install_pplacer.sh \
-	--prefix $venv \
+	--prefix $VENV \
 	--srcdir ./src \
 	--pplacer-version $PPLACER_VERSION \
 	--opamroot $opamroot
@@ -104,21 +129,18 @@ fi
 
 # install infernal and easel binaries
 INFERNAL=infernal-${INFERNAL_VERSION}-linux-intel-gcc
-venv_abspath=$(readlink -f $venv)
-if [ ! -f $venv/bin/cmalign ]; then
+venv_abspath=$(readlink -f $VENV)
+if [ ! -f $VENV/bin/cmalign ]; then
     mkdir -p src && \
 	(cd src && \
 	wget -N http://selab.janelia.org/software/infernal/${INFERNAL}.tar.gz && \
 	for binary in cmalign cmconvert esl-alimerge esl-sfetch esl-reformat; do
 	    tar xvf ${INFERNAL}.tar.gz --no-anchored binaries/$binary
 	done && \
-	    cp ${INFERNAL}/binaries/* ../$venv/bin && \
+	    cp ${INFERNAL}/binaries/* ../$VENV/bin && \
 	    rm -r ${INFERNAL}
 	)
 fi
 
-# install other python packages
-pip install --allow-external argparse -r $YAPP/requirements.txt
-
 # correct any more shebang lines
-$PYTHON src/virtualenv-${VENV_VERSION}/virtualenv.py --relocatable $venv
+virtualenv --relocatable $VENV
