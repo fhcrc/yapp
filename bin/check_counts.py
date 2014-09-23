52abf36eb81c5e05383278735a150b0cc938269d
Noah Hoffman <noah.hoffman@gmail.com>
2014-09-22 17:24:40 -0700

    add --permissive option to check_counts.py

--- bin/check_counts.py
+++ bin/check_counts.py
@@ -15,12 +15,15 @@ from collections import Counter, defaultdict
 
 log = logging.getLogger(__name__)
 
+
 def get_args(arguments):
     parser = argparse.ArgumentParser()
     parser.add_argument('seq_info', type=argparse.FileType('r'))
     parser.add_argument('by_specimen', type=argparse.FileType('r'))
     parser.add_argument('--weights', type=argparse.FileType('r'),
                         help="optional weights file to adjust counts in seq_info")
+    parser.add_argument('--permissive', action='store_true', default=False,
+                        help="Be more permissive (use with non-integer weights)")
     parser.add_argument('-o', '--output', type=argparse.FileType('w'),
                         default=sys.stdout)
     return parser.parse_args(arguments)
@@ -35,7 +38,7 @@ def main(arguments):
     seq_info = csv.reader(args.seq_info)
     if args.weights:
         # column 2 (index 1) should correspond to names of sequences in seq_info
-        weights = {row[1]: int(row[2]) for row in csv.reader(args.weights)}
+        weights = {row[1]: float(row[2]) for row in csv.reader(args.weights)}
         expected = defaultdict(int)
         for seqname, specimen in seq_info:
             expected[specimen] += weights[seqname]
@@ -52,11 +55,24 @@ def main(arguments):
     status = [set(expected.keys()) == set(observed.keys())]
 
     writer = csv.writer(args.output)
-    writer.writerow(['specimen', 'expected', 'observed', 'status'])
+    writer.writerow(['specimen', 'expected', 'observed', 'delta_pct', 'status'])
     for specimen in set(expected.keys() + observed.keys()):
         ex, ob = expected[specimen], observed.get(specimen, 0)
-        status.append(ex == ob)
-        writer.writerow([specimen, ex, ob, 'ok' if ex == ob else 'ERROR'])
+        delta_pct = abs(100.0 * (ob - ex)/float(ex))
+
+        if args.permissive:
+            # be more permissive when masses are small
+            if ex < 100:
+                specimen_ok = delta_pct < 50.0
+            elif ex < 1000:
+                specimen_ok = delta_pct < 10.0
+            else:
+                specimen_ok = delta_pct < 1.0
+        else:
+            specimen_ok = ex == ob
+
+        status.append(specimen_ok)
+        writer.writerow([specimen, ex, ob, delta_pct, 'ok' if specimen_ok else 'ERROR'])
 
     exit_status = 0 if all(status) else 1
     if exit_status:
