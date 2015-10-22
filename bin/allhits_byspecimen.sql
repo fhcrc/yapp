8eb44d7e356c7459bc68c590d7d13fd99631688c
Noah Hoffman <noah.hoffman@gmail.com>
2015-10-22 09:51:04 -0700

    add summary of all hits by specimen

---
+++ bin/allhits_byspecimen.sql
@@ -0,0 +1,16 @@
+select
+w.name as otu_rep,
+w.name1 as specimen_rep,
+s.specimen,
+w.abundance,
+h.pct_id,
+c.tax_name as classif_name,
+c.rank,
+r.*
+from classif c
+join hits h on c.name = h.query
+join ref_info r on h.target = r.seqname
+join weights w on w.name = c.name
+join seq_info s on s.name = w.name1
+where c.abundance >= MIN_MASS  -- total abundance
+order by classif_name, c.abundance desc;
