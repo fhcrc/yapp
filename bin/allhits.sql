e1d27ebfb60e9efaee494ad883288949b6341d9f
Noah Hoffman <noah.hoffman@gmail.com>
2017-05-15 16:16:04 -0700

    include annotation for non-ncbi references

--- bin/allhits.sql
+++ bin/allhits.sql
@@ -10,10 +10,11 @@ c.rank,
 r.seqname,
 r.version,
 r.tax_id,
-r.description,
+coalesce(r.description, r.organism) as description_or_organism,
 r.length,
 r.ambig_count,
-r.is_type
+r.is_type,
+r.collection
 from classif c
 join hits h on c.name = h.query
 join ref_info r on h.target = r.seqname
