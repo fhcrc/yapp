f9c6531abfe52711675fbb8eb5f02ef04c217783
Noah Hoffman <noah.hoffman@gmail.com>
2017-07-06 09:38:01 -0700

    add scripts and notes for analysis

---
+++ bin/knit.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+if(Sys.getenv("VIRTUAL_ENV") == ""){ stop("An active virtualenv is required") }
+source(file.path(Sys.getenv('VIRTUAL_ENV'), 'bin', 'rvenv'))
+
+suppressPackageStartupMessages(library(argparse, quietly = TRUE))
+suppressPackageStartupMessages(library(rmarkdown, quietly = TRUE))
+
+
+main <- function(arguments){
+  parser <- ArgumentParser()
+  parser$add_argument('infile')
+  parser$add_argument('-o', '--outfile')
+  parser$add_argument('-f', '--format', choices=c('html', 'pdf', 'all'),
+                      default='html', help='output format [default %(default)s]')
+  parser$add_argument('-e', '--extra-args', help='quoted string of ignored extra arguments')
+  args <- parser$parse_args(arguments)
+
+  format <- if(args$format == 'all'){
+              'all'
+            }else{
+              gettextf('%s_document', args$format)
+            }
+
+  rmarkdown::render(
+      args$infile,
+      output_file=args$outfile,
+      output_format=format)
+
+}
+
+main(commandArgs(trailingOnly=TRUE))
+## invisible(warnings())
+
+
