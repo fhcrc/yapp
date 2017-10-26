716e440a38a573bb3037444ba2ff9c131f8ff004
Noah Hoffman <noah.hoffman@gmail.com>
2017-10-26 16:14:39 -0700

    update transfer script to allow transfer to alternative location

--- bin/transfer_to_bvdiversity.sh
+++ bin/transfer_to_bvdiversity.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+to_dir=/fh/fast/fredricks_d/bvdiversity/$(date "+%Y-%m-%d")-$(basename $(pwd))
+
+source $(dirname $0)/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('--from-dir', default="output")
+parser.add_argument('--to-dir', default="$to_dir")
+parser.add_argument('-n', '--dry-run', action="store_true")
+EOF
+
+echo "from directory: $FROM_DIR"
+echo "to directory: $TO_DIR"
+if [[ $DRY_RUN ]]; then
+    bin/transfer.py "$FROM_DIR/for_transfer.txt" --dest "$TO_DIR" -n
+    exit
+fi
+
 if ! git diff-index --quiet HEAD; then
     echo "Error: git repo is dirty - commit and try again"
     echo
@@ -9,11 +25,7 @@ if ! git diff-index --quiet HEAD; then
     exit 1
 fi
 
-dirname=${1-$(date "+%Y-%m-%d")-$(basename $(pwd))}
+mkdir -p "$TO_DIR"
+(git --no-pager log -n1; git status) > "$TO_DIR/project_status.txt"
+bin/transfer.py "$FROM_DIR/for_transfer.txt" --dest "$TO_DIR"
 
-dest_dir=/fh/fast/fredricks_d/bvdiversity/$dirname
-
-mkdir -p "$dest_dir"
-
-(git --no-pager log -n1; git status) > $dest_dir/project_status.txt
-bin/transfer.py output/for_transfer.txt --dest $dest_dir
