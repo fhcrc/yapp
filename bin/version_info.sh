070f0ed38614f143d5bace8e26b41ab847fb3eae
Noah Hoffman <noah.hoffman@gmail.com>
2017-11-08 21:10:29 -0800

    modify a few more taxonomic names

--- bin/version_info.sh
+++ bin/version_info.sh
@@ -5,25 +5,8 @@ pwd
 git status
 echo
 
-grep -C3 -E '^refpkg =' SConstruct
+git remote -v
+echo
 
 git --no-pager log -n 1
 echo
-
-which pplacer
-pplacer --version
-echo
-
-which cmalign
-cmalign -h | grep '#'
-echo
-
-which vsearch
-vsearch --version
-echo
-
-which FastTree
-FastTree -expert 2>&1 | head -1
-echo
-
-pip freeze
