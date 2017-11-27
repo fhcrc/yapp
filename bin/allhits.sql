2cb265abfcadd5162e842cefaf78498ec6e5d1ed
Noah Hoffman <noah.hoffman@gmail.com>
2017-11-27 11:57:23 -0800

    use ncbi-plus in example for SConstruct-get-details

--- bin/allhits.sql
+++ bin/allhits.sql
@@ -1,16 +1,12 @@
 select
-c.name as otu_rep,
-c.abundance,
+c.name as sv,
+c.rank,
 h.pct_id,
 c.tax_name as classif_name,
-c.rank,
--- r.*
--- r.organism,
--- r.date,
+coalesce(r.description, r.organism) as hit_desc,
 r.seqname,
 r.version,
 r.tax_id,
-coalesce(r.description, r.organism) as description_or_organism,
 r.length,
 r.ambig_count,
 r.is_type,
@@ -18,5 +14,5 @@ r.collection
 from classif c
 join hits h on c.name = h.query
 join ref_info r on h.target = r.seqname
-where abundance >= MIN_MASS
-order by classif_name, abundance desc;
+where c.want_rank = 'species'
+order by c.name;
--- bin/allhits_byspecimen.sql
+++ bin/allhits_byspecimen.sql
@@ -1,18 +1,15 @@
 select
-w.name as otu_rep,
+w.name as sv,
 w.name1 as specimen_rep,
-s.specimen,
 w.abundance,
+s.specimen,
+c.rank,
 h.pct_id,
 c.tax_name as classif_name,
-c.rank,
--- r.*
--- r.organism,
--- r.date,
+coalesce(r.description, r.organism) as description_or_organism,
 r.seqname,
 r.version,
 r.tax_id,
-coalesce(r.description, r.organism) as description_or_organism,
 r.length,
 r.ambig_count,
 r.is_type,
@@ -22,5 +19,5 @@ join hits h on c.name = h.query
 join ref_info r on h.target = r.seqname
 join weights w on w.name = c.name
 join seq_info s on s.name = w.name1
-where c.abundance >= MIN_MASS  -- total abundance
-order by classif_name, c.abundance desc;
+where c.want_rank = 'species'
+order by sv, w.abundance desc;
