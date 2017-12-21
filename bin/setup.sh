c5b40b2f094c19f0d7c78ee3972ff9b99162c3eb
Noah Hoffman <noah.hoffman@gmail.com>
2017-12-20 16:37:58 -0800

    install pplacer binaries to virtualenv

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
 PYTHON=${PYTHON-python3}
-venv="py3-env"
+venv="$(pwd)/py3-env"
 
 if [[ -d $venv ]]; then
     echo "$venv already exists"
-    exit 1
+else
+    $PYTHON -m venv "$venv"
 fi
-
-$PYTHON -m venv "$venv"
 
 while read pkg; do
     "$venv/bin/pip" install "$pkg" --upgrade
 done < requirements.txt
 
 # if --relocatable becomes necessary: https://gist.github.com/wynemo/3310583
+
+# install pplacer and accompanying python scripts
+PPLACER_BUILD=1.1.alpha19
+PPLACER_DIR=pplacer-Linux-v${PPLACER_BUILD}
+PPLACER_ZIP=${PPLACER_DIR}.zip
+PPLACER_GH="https://github.com/matsen/pplacer"
+
+pplacer_is_installed(){
+    $venv/bin/pplacer --version 2> /dev/null | grep -q "$PPLACER_BUILD"
+}
+
+if pplacer_is_installed; then
+    echo -n "pplacer is already installed: "
+    $venv/bin/pplacer --version
+else
+    mkdir -p src && \
+	(cd src && \
+	wget -nc --quiet $PPLACER_GH/releases/download/v$PPLACER_BUILD/$PPLACER_ZIP && \
+	unzip -o $PPLACER_ZIP && \
+	cp $PPLACER_DIR/{pplacer,guppy,rppr} $venv/bin)
+    # confirm that we have installed the requested build
+    if ! pplacer_is_installed; then
+	echo -n "Error: you requested pplacer build $PPLACER_BUILD "
+	echo "but $($venv/bin/pplacer --version) was installed."
+	echo "Try removing src/$PPLACER_ZIP first."
+	exit 1
+    fi
+fi
