4e8ce5fb13e64d800111ab1243df4b4f0d2a7003
Noah Hoffman <noah.hoffman@gmail.com>
2023-03-23 13:42:53 -0700

    push image to ghcr.io

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -2,13 +2,16 @@
 
 # build and deploy the yapp Docker image
 
-TAG=${TAG-$(git log -1 --pretty=format:%h)}
+TAG=${TAG-$(git describe --tags --dirty)}
 
 echo "building Docker image with tag $TAG"
 docker build \
        --platform=linux/amd64 . \
-       -t yapp
-       # -t "docker.labmed.uw.edu/uwlabmed/userbase:${TAG:?}" \
-       # -t "docker.labmed.uw.edu/uwlabmed/userbase:latest" \
-       # --build-arg VERSION="$TAG"
+       -t yapp \
+       -t ghcr.io/fhcrc/yapp:latest \
+       -t ghcr.io/fhcrc/yapp:$TAG
 
+if [[ $1 = "push" ]]; then
+    docker push ghcr.io/fhcrc/yapp:latest
+    docker push ghcr.io/fhcrc/yapp:$TAG
+fi
