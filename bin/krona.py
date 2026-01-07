05b35513ddf8c6da7494c092ae6bc02c8dacbae3
Chris Rosenthal <crosenth@uw.edu>
2026-01-07 11:16:21 -0800

    Adding script to convert taxon_table_long.csv to krona plot compatible input

---
+++ bin/krona.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+Creates a tsv file that can be used with KronaTools ktImportText
+
+https://github.com/marbl/Krona/wiki/Importing-text-and-XML-data
+"""
+
+import argparse
+import csv
+import itertools
+import os
+import sys
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'taxon_table_long',
+        help='csv with columns: specimen,tax_name,rank,read_count',
+        type=argparse.FileType('r')
+        )
+    parser.add_argument(
+        'taxtable',
+        type=argparse.FileType('r')
+        )
+    parser.add_argument(
+        '--drop-no-rank',
+        action='store_true',
+        help='Drop "no rank" taxonomies'
+        )
+    parser.add_argument('outdir')
+    args = parser.parse_args(arguments)
+    taxtable = csv.DictReader(args.taxtable)
+    header = taxtable.fieldnames
+    ranks = header[header.index('root'):]
+    if args.drop_no_rank:
+        ranks = [r for r in ranks if not r.endswith('_')]
+    taxtable = {t['tax_id']: t for t in taxtable}
+    names = {}
+    for row in taxtable.values():
+        name_row = []
+        for r in ranks:
+            name_row.append(taxtable[row[r]]['tax_name'] if row[r] else '')
+        names[row['tax_name']] = name_row
+    taxon_table_long = csv.DictReader(args.taxon_table_long)
+    taxon_table_long = sorted(taxon_table_long, key=lambda x: x['specimen'])
+    taxon_table_long = itertools.groupby(
+        taxon_table_long,
+        key=lambda x: x['specimen']
+        )
+    os.makedirs(args.outdir, exist_ok=True)
+    for specimen, rows in taxon_table_long:
+        with open(os.path.join(args.outdir, specimen + '.tsv'), 'w') as flo:
+            for r in rows:
+                tax_name = r['tax_name'].split('/')[0]
+                out = csv.writer(flo, delimiter='\t')
+                lineage = names[tax_name].copy()
+                lineage[lineage.index(tax_name)] = r['tax_name']
+                out.writerow([r['read_count']] + lineage)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
